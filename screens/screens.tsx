fcda772f07f9f4ef00fffe09da7bfc8e47fa0140
Psalishol samuel <psalishol80@gmail.com>
2022-08-08 12:14:07 +0100

    Screens Index

---
+++ screens/screens.tsx
@@ -0,0 +1,25 @@
+import ChatRoomScreen from "./ChatRoomScreen";
+import HomeScreen from "./HomeScreen";
+import InboxScreen from "./InboxScreen";
+import MessagesScreen from "./MessagesScreen";
+import ProfileScreen from "./ProfileScreen";
+import ProjectScreen from "./ProjectScreen";
+import ConfirmRegistrationScreen from "./Auth/ConfirmRegistrationScreen";
+import ForgotPassword from "./Auth/ForgotPassword";
+import OnBoardingScreen from "./Auth/OnBoardingScreen";
+import SignInScreen from "./Auth/SignInScreen";
+import SignUpScreen from "./Auth/SignUpScreen";
+
+export {
+  ChatRoomScreen,
+  HomeScreen,
+  InboxScreen,
+  MessagesScreen,
+  ProfileScreen,
+  ProjectScreen,
+  ConfirmRegistrationScreen,
+  ForgotPassword,
+  OnBoardingScreen,
+  SignInScreen,
+  SignUpScreen,
+};
