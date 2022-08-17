09b93e974b9b1ffbcdf6e14d3b135f4babc88ecb
Psalishol samuel <psalishol80@gmail.com>
2022-08-17 17:36:22 +0100

    Implemented real time chat

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -7,29 +7,84 @@ import {
   Image,
   TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChatTextInput from "../components/ChatRoom/MessageInputContainer/MessageInputComponent";
 import Messages from "../components/ChatRoom/MessageContainer/MessageContainer";
 import chats from "../DummyData/Chats";
 import CustomStatusBar from "../components/GlobalComponents/CustomStatusBar/CustomStatusBar";
 import { AntDesign, Entypo } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
-
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Message } from "../src/models";
+import { Auth, DataStore, Storage } from "aws-amplify";
+import { User } from "../src/models";
+// import { S3Image } from "aws-amplify-react-native";
+import { colors } from "../constants/Colors";
+import { ChatRoomHeader } from "../components/ChatRoom/HeaderComponent/ChatRoomHeader";
 export default function ChatRoomScreen() {
+  const [chatRoomMessages, setChatRoomMessages] = useState<Message[]>([]);
+  const [user, setUser] = useState<User>();
+
+  // console.log(chatRoomMessages);
+
+  const route = useRoute();
+
+  const chatRoomID = route?.params?.id;
+  const userId = route?.params?.userId;
+  const recipientName = route.params?.name;
+  const recipientProfileImageUri = route.params?.profileImageUri;
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const messages = (
+        await DataStore.query(Message, (messages) =>
+          messages.chatroomID("eq", chatRoomID)
+        )
+      ).sort((a: Message, b: Message) => {
+        const firstMessageCreation = new Date(a.updatedAt).getTime();
+        const secondMessageCreation = new Date(b.updatedAt).getTime();
+        if (firstMessageCreation < secondMessageCreation) return 1;
+        if (firstMessageCreation > secondMessageCreation) return -1;
+        return 0;
+      });
+      setChatRoomMessages(messages);
+    };
+    fetchMessages();
+  }, []);
+
+  useEffect(() => {
+    const subscription = DataStore.observe(Message).subscribe((msg) => {
+      if (msg.model === Message && msg.opType === "INSERT") {
+        console.log(msg.model, msg.opType, msg.element);
+        setChatRoomMessages((existingChatroomMessages) => [
+          msg.element,
+          ...existingChatroomMessages,
+        ]);
+      }
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!chatRoomMessages && !user) {
+    console.log("Messages loading or no message in this chatroom.");
+    return;
+  }
   return (
     <>
       <CustomStatusBar barStyle="dark-content" backgroundColor={"#3872E9"} />
-      {/* */}
       <SafeAreaView style={styles.page}>
-        <ChatRoomHeader />
-        <View style={{ flex: 1 }} />
+        <ChatRoomHeader
+          profileImageUri={recipientProfileImageUri}
+          name={recipientName}
+        />
         <FlatList
-          style={{}}
-          data={chats.messages}
-          renderItem={({ item }) => <Messages chat={item} />}
+          inverted
+          data={chatRoomMessages}
+          renderItem={({ item }) => (
+            <Messages recipientID={userId} message={item} />
+          )}
           ListFooterComponent={() => <View style={{ height: 20 }}></View>}
         />
-        <ChatTextInput />
+        <ChatTextInput chatRoomId={chatRoomID} />
       </SafeAreaView>
     </>
   );
@@ -41,43 +96,3 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
-
-export const ChatRoomHeader = () => {
-  const navigation = useNavigation();
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        backgroundColor: "#3872E9",
-        padding: 10,
-      }}
-    >
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <AntDesign name="arrowleft" size={24} color="white" />
-      </TouchableOpacity>
-      <Image
-        style={{ height: 40, width: 40, borderRadius: 40, marginLeft: 10 }}
-        source={{
-          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
-        }}
-      />
-
-      <Text
-        style={{
-          fontSize: 16,
-          color: "white",
-          fontWeight: "bold",
-          marginLeft: 10,
-          flex: 1,
-        }}
-      >
-        Psalishol Samuel
-      </Text>
-
-      <TouchableOpacity>
-        <Entypo name="dots-three-vertical" size={20} color="white" />
-      </TouchableOpacity>
-    </View>
-  );
-};
