f04c73a552cb6dd47e6710257dc9681339b8c69e
Psalishol samuel <psalishol80@gmail.com>
2022-08-18 07:59:05 +0100

    Implemented image selection from library

--- components/ChatRoom/MessageInputContainer/MessageInputComponent.tsx
+++ components/ChatRoom/MessageInputContainer/MessageInputComponent.tsx
@@ -1,11 +1,25 @@
-import { View, Text, TextInput, StyleSheet, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Pressable,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from "react-native";
 import React, { useState } from "react";
-import { SimpleLineIcons, Feather, Ionicons } from "@expo/vector-icons";
+import { SimpleLineIcons, Feather, Ionicons, Entypo } from "@expo/vector-icons";
 import { Auth, DataStore } from "aws-amplify";
 import { Message } from "../../../src/models";
+import EmojiSelector from "react-native-emoji-selector";
+import * as ImagePicker from "expo-image-picker";
 
-export default function ChatTextInput({ chatRoomId }: any) {
+export default function ChatTextInput({ chatRoomId }: { chatRoomId: string }) {
   const [message, setMessage] = useState("");
+  const [isEmojiInputOpened, setIsEmojiInputOpened] = useState<boolean>(false);
+  const [pickedImage, setPickedImage] = useState<String>();
+
+  console.log(message);
 
   const onSendMessage = async (message: string) => {
     const authUser = await Auth.currentAuthenticatedUser();
@@ -20,6 +34,18 @@ export default function ChatTextInput({ chatRoomId }: any) {
       .catch((error) => console.log("Error in sending message", error));
   };
 
+  const pickImage = async () => {
+    const pickedImage = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: false,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    if (!pickedImage.cancelled) {
+      setPickedImage(pickedImage.uri);
+    }
+  };
   const onPlusClicked = () => {
     console.warn("plus button clicked");
   };
@@ -33,26 +59,32 @@ export default function ChatTextInput({ chatRoomId }: any) {
   return (
     <View style={styles.inputContainer}>
       <View style={styles.textInputContainer}>
-        <SimpleLineIcons name="emotsmile" size={24} color="darkgrey" />
+        <TouchableOpacity
+          onPress={() => setIsEmojiInputOpened(!isEmojiInputOpened)}
+        >
+          <SimpleLineIcons name="emotsmile" size={24} color="darkgrey" />
+        </TouchableOpacity>
         <TextInput
           value={message}
           onChangeText={setMessage}
           placeholder="Message"
           style={styles.input}
         />
-        <Feather
-          name="camera"
-          size={24}
-          color="darkgrey"
-          style={styles.icons}
-        />
+        <TouchableOpacity onPress={pickImage}>
+          <Feather
+            name="camera"
+            size={24}
+            color="darkgrey"
+            style={styles.icons}
+          />
+        </TouchableOpacity>
         <Feather name="mic" size={24} color="darkgrey" />
       </View>
       <Pressable onPress={onPressed} style={styles.sendButton}>
         {message ? (
-          <Ionicons name="ios-send" size={20} color="white" />
+          <Ionicons name="ios-send" size={18} color="white" />
         ) : (
-          <Text style={styles.buttonText}>+</Text>
+          <Entypo name="plus" size={24} color="white" />
         )}
       </Pressable>
     </View>
@@ -74,19 +106,22 @@ const styles = StyleSheet.create({
   textInputContainer: {
     flex: 1,
     flexDirection: "row",
-    backgroundColor: "lightgrey",
+    backgroundColor: "#f0f0f0",
     borderRadius: 30,
     justifyContent: "center",
     padding: 5,
     marginRight: 5,
     marginLeft: 10,
     paddingHorizontal: 10,
+    paddingVertical: 5,
+    zIndex: 6,
   },
   inputContainer: {
     flexDirection: "row",
     marginBottom: 10,
     justifyContent: "center",
     alignItems: "center",
+    marginTop: 10,
   },
   sendButton: {
     width: 40,
